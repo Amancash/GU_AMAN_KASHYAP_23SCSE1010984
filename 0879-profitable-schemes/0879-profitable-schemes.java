33bf750124aa207bfd23cee2cea9f628ba3851da
Amancash <kashyap20aman@gmail.com>
2026-01-15 13:08:26 +0530

    Time: 83 ms (48.13%), Space: 61.8 MB (44.4%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,3 +1,33 @@
+// class Solution {
+//     int[][][] dp;
+//     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+//         dp = new int[group.length][n + 1][minProfit + 1];
+
+//         for (int i = 0; i < group.length; i++) {
+//             for (int j = 0; j <= n; j++) {
+//                 for (int k = 0; k <= minProfit; k++) {
+//                     dp[i][j][k] = -1;
+//                 }
+//             }
+//         }
+//         return solve (n,minProfit,group , profit,group.length-1);
+//     }
+//     public int solve(int n, int minProfit, int[] group, int[] profit,int i){
+//         if(i < 0) {
+//             return minProfit == 0 ? 1 : 0;
+//         }   
+//         if(dp[i][n][minProfit]!=-1) return dp[i][n][minProfit];
+//         int not = solve(n,minProfit,group , profit,i-1);
+//         int take = 0;
+//         if(n>=group[i]){
+//         minProfit = Math.max(0, minProfit - profit[i]);
+//             take = solve(n-group[i],minProfit,group , profit,i-1);
+//         }
+//         return dp[i][n][minProfit]= take+not;
+        
+//     }
+// }
+
 class Solution {
     static final int MOD = 1000000007;
     int[][][] dp;
