3d2ed0f5795bb1c420af8fa53c49e9210513f130
Amancash <kashyap20aman@gmail.com>
2026-01-15 13:07:52 +0530

    Time: 80 ms (48.55%), Space: 62.2 MB (41.08%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -0,0 +1,44 @@
+class Solution {
+    static final int MOD = 1000000007;
+    int[][][] dp;
+
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        dp = new int[group.length][n + 1][minProfit + 1];
+
+        for (int i = 0; i < group.length; i++) {
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= minProfit; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+
+        return solve(group.length - 1, n, minProfit, group, profit);
+    }
+
+    int solve(int i, int members, int profitLeft, int[] group, int[] profit) {
+        if (i < 0) {
+            return profitLeft == 0 ? 1 : 0;
+        }
+
+        if (dp[i][members][profitLeft] != -1)
+            return dp[i][members][profitLeft];
+
+        // NOT TAKE
+        int ways = solve(i - 1, members, profitLeft, group, profit);
+
+        // TAKE
+        if (members >= group[i]) {
+            int newProfit = Math.max(0, profitLeft - profit[i]);
+            ways = (ways + solve(
+                    i - 1,
+                    members - group[i],
+                    newProfit,
+                    group,
+                    profit
+            )) % MOD;
+        }
+
+        return dp[i][members][profitLeft] = ways;
+    }
+}
