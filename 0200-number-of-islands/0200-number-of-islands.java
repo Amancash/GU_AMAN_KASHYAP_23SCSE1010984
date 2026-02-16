fc9182c29155cea49dfc3f5aaa71e8801a6c4257
Amancash <kashyap20aman@gmail.com>
2026-02-16 16:17:41 +0530

    Time: 4 ms (48.86%), Space: 52.3 MB (67.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,31 @@
 class Solution {
-    int m, n;
-    boolean[][] visited;
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-    
+    boolean[][] vis;
     public int numIslands(char[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        visited = new boolean[m][n];
-        int count = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(grid, i, j);
-                    count++;   // one island completed
+        int n = grid.length;
+        int m = grid[0].length;
+        vis = new boolean[n][m];
+        int count=0;
+        for(int i =0 ; i <n ; i ++){
+            for(int j =0 ; j <m ; j ++){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    count++;
+                    dfs(i,j,grid);
                 }
-            }
+        } 
         }
         return count;
-    }
-    
-    private void dfs(char[][] grid, int x, int y) {
-        visited[x][y] = true;
         
-        for (int[] d : dirs) {
-            int nx = x + d[0];
-            int ny = y + d[1];
-            
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n)
-                continue;
-            
-            if (grid[nx][ny] == '1' && !visited[nx][ny]) {
-                dfs(grid, nx, ny);
-            }
-        }
+    }
+    public void dfs(int i , int j ,char[][] grid){
+        int n = grid.length;
+        int m = grid[0].length;
+        if (i < 0 || j < 0 || i >= n || j >= m) return;
+        if (grid[i][j] == '0' || vis[i][j]) return;
+        vis[i][j]=true;
+        dfs(i + 1, j, grid);
+        dfs(i - 1, j, grid);
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
     }
 }
 
