3628437e3aed998d95babf82b993023d1079a891
Amancash <kashyap20aman@gmail.com>
2026-08-05 16:21:26 +0530

    Time: 343 ms (11.74%), Space: 0B (100%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select id , movie , description , rating
+from Cinema
+where Cinema.id %2=1
+
+   and Cinema.description != 'boring'
+   order by rating desc;
