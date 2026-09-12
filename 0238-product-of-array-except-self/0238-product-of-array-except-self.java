8e7170d8ec5cbe1c180cd05a352b9f348853f5e0
Amancash <kashyap20aman@gmail.com>
2026-09-12 11:51:31 +0530

    Time: 2 ms (93.13%), Space: 63.3 MB (98.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,18 +1,54 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] ans = new int[nums.length];
         int n = nums.length;
-        ans[0] = 1;
-        for(int i = 1; i < n; i++){
-            ans[i] = ans[i - 1] * nums[i - 1];
-        }
-        int right = 1;
-        for(int i = n - 1; i >= 0; i--){
-            ans[i] = ans[i] * right;
-            right = right * nums[i];
-        }
+        int[] pre = new int[n];
+        int[] suf = new int[n];
 
-        return ans;
+        pre[0]=1;
+        for(int i =1 ; i<n ; i++){
+            pre[i]=pre[i-1]*nums[i-1];
+        }
+        suf[n-1]=1;
+        for(int i = n-2 ; i>=0 ;i--){
+            suf[i]=suf[i+1]*nums[i+1];
+        }
+        for(int i =0 ; i<n ;i++){
+            nums[i]=pre[i]*suf[i];
+        }
+        return nums;
+        // int n = nums.length ;
+        // int prod =1;
+        // for(int x : nums){
+        //     prod*=x;
+        // }
+        // for(int i =0 ; i<n ;i++){
+        //     nums[i]=prod/nums[i];
+        // }
+        // return nums;
+        // int[] ans = new int[nums.length];
+        // int n = nums.length;
+        // ans[0] = 1;
+        // for(int i = 1; i < n; i++){
+        //     ans[i] = ans[i - 1] * nums[i - 1];
+        // }
+        // int right = 1;
+        // for(int i = n - 1; i >= 0; i--){
+        //     ans[i] = ans[i] * right;
+        //     right = right * nums[i];
+        // }
+        // return ans;
 
+
+        // int[] ans = new int[nums.length];
+        // ans[0]=1;
+        // for(int i =1 ; i < nums.length ;i++){
+        //     ans[i] = nums[i-1]*ans[i-1];
+        // }
+        // int right =1;
+        // for(int i =nums.length-1 ; i>=0 ;i--){
+        //     ans[i]=ans[i]*right;
+        //     right = right*nums[i];
+        // }
+        // return ans;
     }
 }
